9ce035fa41ef5088b52094f82a7cf653afe7d54a
yesolee <58902084+yesolee@users.noreply.github.com>
2025-02-01 11:08:25 +0900

    Revert "Apiurl"

--- back/models.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from db import Base
-
-class Counter(Base):
-    __tablename__ = "counter"
-    id = Column(Integer, primary_key=True)
-    count = Column(Integer)
--- back/schemas.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-
-class Action(BaseModel):
-    action: str
